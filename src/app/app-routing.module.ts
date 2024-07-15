33bbbaa2683e28d84cf5735e8bbf06b852013557
AdamBh22 <145387443+AdamBh22@users.noreply.github.com>
2024-07-15 16:57:08 +0100

    Revert "add sign in & sign up pages"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LoginComponent} from "./pages/login/login.component";
-import {SignupComponent} from "./pages/signup/signup.component";
-import {LnavbarComponent} from "./pages/lnavbar/lnavbar.component";
 
-const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'signup',
-    component: SignupComponent
-  },
-  {
-    path:'',
-    redirectTo:'/login',
-    pathMatch: 'full',
-  },
-  {
-    path:'lnavbar',
-    component:LnavbarComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
